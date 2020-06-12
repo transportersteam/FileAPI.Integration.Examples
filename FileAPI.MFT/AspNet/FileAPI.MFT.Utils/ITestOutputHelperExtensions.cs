3cc3cfbc012b475836d2e203f482e9eca526a896
Alberto Infante Roldan <AlbertoInf@youforce.net>
2020-06-12 12:59:38 +0200

    #VRFTR-350: [AspNet] List examples.

--- FileAPI.MFT/AspNet/FileAPI.MFT.Utils/ITestOutputHelperExtensions.cs
+++ FileAPI.MFT/AspNet/FileAPI.MFT.Utils/ITestOutputHelperExtensions.cs
@@ -1,4 +1,7 @@
-﻿using Newtonsoft.Json;
+﻿using Ftaas.Sdk.Base;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
 using Xunit.Abstractions;
 
 namespace FileAPI.MFT.Utils
@@ -8,6 +11,18 @@ namespace FileAPI.MFT.Utils
         public static void WriteJson(this ITestOutputHelper output, object value)
         {
             output.WriteLine(JsonConvert.SerializeObject(value, Formatting.Indented));
+        }
+        public static void WriteJsonPaginatedItemsWithoutData<T>(this ITestOutputHelper output, PaginatedItems<T> value)
+        {
+            var truncatedValue = new PaginatedItems<string>
+            {
+                Data = new List<string> { $"Returned files: {value.Data.Count()}. They are not shown in the tests because if they are too much the readability would be bad." },
+                PageIndex = value.PageIndex,
+                PageSize = value.PageSize,
+                Count = value.Count
+            };
+
+            output.WriteLine(JsonConvert.SerializeObject(truncatedValue, Formatting.Indented));
         }
 
         public static void WriteTittle(this ITestOutputHelper output, string value)
