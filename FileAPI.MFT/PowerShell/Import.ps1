1f7b161c5a067a2ce7ddf8be4b9d108a05bdcb0d
Martin Ilinov Martinov <martinmar@youforce.net>
2020-06-12 16:59:58 +0200

    powershell add librariesnet45

--- FileAPI.MFT/PowerShell/Import.ps1
+++ FileAPI.MFT/PowerShell/Import.ps1
@@ -3,7 +3,6 @@ Write-Host "Importing libraries..." -ForegroundColor Green
 Import-Module .\libraries\Newtonsoft.Json.dll
 Import-Module .\libraries\Microsoft.Extensions.Logging.Abstractions.dll
 Import-Module .\libraries\Microsoft.Extensions.Logging.dll
-Import-Module .\libraries\Microsoft.Extensions.Http.dll
 Import-Module .\libraries\Microsoft.Extensions.DependencyInjection.Abstractions.dll
 Import-Module .\libraries\Microsoft.Extensions.DependencyInjection.dll
 Import-Module .\libraries\Ftaas.Sdk.Base.dll
