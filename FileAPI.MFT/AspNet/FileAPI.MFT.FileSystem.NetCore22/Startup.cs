6fe17c56c28e8f1cc3010e907fab38be13267e21
Alberto Infante Roldan <AlbertoInf@youforce.net>
2020-06-16 11:06:06 +0200

    #VRFTR-350: [AspNet] Removed most of ToDo.

--- FileAPI.MFT/AspNet/FileAPI.MFT.FileSystem.NetCore22/Startup.cs
+++ FileAPI.MFT/AspNet/FileAPI.MFT.FileSystem.NetCore22/Startup.cs
@@ -37,8 +37,7 @@ namespace FileAPI.MFT.FileSystem.NetCore22
                     // Here comes the logic of retrieving the authentaction token.
                     // This logic is out of the File API scope, but as a guide, you can use something similar
                     // to what is implemented in Examples/RetrieveToken.cs.
-                    //return await Task.FromResult("MyToken"); ToDo
-                    return await Task.FromResult("eyJhbGciOiJSUzI1NiIsImtpZCI6IlNELVQtSUFNV0VCMDgucmFldC5sb2NhbCJ9.eyJzY29wZSI6W10sImNsaWVudF9pZCI6IktZTHJHT0tucUc3cHBJQkVyeW9Vdzhnd2VObWQ0WmJuIiwiaXNzIjoiaWRlbnRpdHkucmFldHRlc3QuY29tIiwiYXBwIjoie1wiaWRcIjpcIjI2OTNjNWE5LTZiMWYtNGE1OS05Y2UxLTExZTU2YTkxYmIzZFwiLFwibmFtZVwiOlwiVmlzbWFSYWV0X1RyYW5zcG9ydGVyc19GVGFhU19TaW5nbGUtVGVuYW50XCJ9IiwiYXV0aHoiOiJ7XCJwZXJtaXNzaW9uc1wiOntcImFsbFwiOmZhbHNlLFwidmFsdWVzXCI6W1wiRnRhYXMuTWFuYWdlRmlsZXNcIl19LFwidGVuYW50c1wiOntcImFsbFwiOmZhbHNlLFwidmFsdWVzXCI6W1wiNjQwMTk3MFwiXX19Iiwiand0dHlwZSI6InN5c3RlbSIsImV4cCI6MTU5MjI5NjIyOX0.Ih1FZmg8GlM8TRqM1ApnfVoJG3HSLhwTBWNz_RNYh6SQa5n54cJautcoKGebGosBg022E3pbzk-unWJmaphVvwfYW23yS500TdWQaLXgxgpo4TX7WX9H8f_0jfDOGWGBjERUhUoflsaYhwtdqCPWB0q-vFGsqoB21pCn2mavElhWXbHs5lNQhoaaE_PN47dccXzCyjQdHSno62JvXvm7wNerGE4WCsjXxifLNnwREi5oS65TWeMQQiGKYESanZFj4Np7N4WB5jjgrSDM74TL3pwtdtsP2A5JbeBHb-9KtMrIXlenj0KKECpoOXTkfJR9yLgNpCB9rRiqGpr7OPijzQ");
+                    return await Task.FromResult("MyToken");
                 });
 
             // Get the FileSystem.SDK service.
--- FileAPI.MFT/AspNet/FileAPI.MFT.Streaming.NetCore22/Startup.cs
+++ FileAPI.MFT/AspNet/FileAPI.MFT.Streaming.NetCore22/Startup.cs
@@ -23,6 +23,7 @@ namespace FileAPI.MFT.Streaming.NetCore22
                 .Build();
 
             // Inject the Streaming.SDK service.
+            // ToDo Write what do the options do.
             var services = new ServiceCollection();
             services.AddStreamingService(
                 options =>
@@ -36,8 +37,7 @@ namespace FileAPI.MFT.Streaming.NetCore22
                     // Here comes the logic of retrieving the authentaction token.
                     // This logic is out of the File API scope, but as a guide, you can use something similar
                     // to what is implemented in Examples/RetrieveToken.cs.
-                    //return await Task.FromResult("MyToken"); ToDo
-                    return await Task.FromResult("eyJhbGciOiJSUzI1NiIsImtpZCI6IlNELVQtSUFNV0VCMDgucmFldC5sb2NhbCJ9.eyJzY29wZSI6W10sImNsaWVudF9pZCI6IktZTHJHT0tucUc3cHBJQkVyeW9Vdzhnd2VObWQ0WmJuIiwiaXNzIjoiaWRlbnRpdHkucmFldHRlc3QuY29tIiwiYXBwIjoie1wiaWRcIjpcIjI2OTNjNWE5LTZiMWYtNGE1OS05Y2UxLTExZTU2YTkxYmIzZFwiLFwibmFtZVwiOlwiVmlzbWFSYWV0X1RyYW5zcG9ydGVyc19GVGFhU19TaW5nbGUtVGVuYW50XCJ9IiwiYXV0aHoiOiJ7XCJwZXJtaXNzaW9uc1wiOntcImFsbFwiOmZhbHNlLFwidmFsdWVzXCI6W1wiRnRhYXMuTWFuYWdlRmlsZXNcIl19LFwidGVuYW50c1wiOntcImFsbFwiOmZhbHNlLFwidmFsdWVzXCI6W1wiNjQwMTk3MFwiXX19Iiwiand0dHlwZSI6InN5c3RlbSIsImV4cCI6MTU5MjIzMjY3Nn0.PBXTrHZEehGTnA4dtREyfsMJxKobl_v4xV0ej3fXbKrnHRsHyfMMGAFnpP7Mbg5cjp2Pi6S9aszOLhKTFU47e22XQdsHiuG4iWuKIMToLce1IBhfdnxD9XWD70kwUGTMSWU1gHqdyStpRB5c5P47XH55HqL0JQ4JuI3GuWuV1t1jin0zFw3NgDqiey0I-utXcQMnZ2m0Yx_lnAtnpUCOoV59alOQzdoygGUgG-J4cMChHuKrIE6oPm5DEHH7CgZfsSQql-d3iBWMPQzccAf4xyj4L9NzGmBTmYJdXnsBYSz-BumBN0hag17UDFeVdtyOn6Jsx4frEFI8EtaLMq6o2g");
+                    return await Task.FromResult("MyToken");
                 });
 
             // Get the Streaming.SDK service.
